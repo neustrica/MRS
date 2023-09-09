988686807e1cb02e147a7399c54c5455a8c2d634
voronichgeorgiy <voronichq@gmail.com>
2023-09-09 14:29:55 +0300

    sqlite бд + оценка треков

--- src/database.py
+++ src/database.py
@@ -1,26 +1,33 @@
 import sqlite3
 
-connection = sqlite3.connect('music_recs.db')  
+connection = sqlite3.connect('music_recs.db') 
 cursor = connection.cursor()
 def save_track_rating(user_id, track_id, rating):
   cursor.execute('''
-    INSERT INTO track_ratings (user_id, track_id, rating)
+    INSERT OR IGNORE INTO track_ratings (user_id, track_id, rating)
     VALUES (?, ?, ?)
   ''', (user_id, track_id, rating))
   
   connection.commit()
+
 def save_album_rating(user_id, track_id, rating):
   cursor.execute('''
-    INSERT INTO album_ratings (user_id, track_id, rating)
+    INSERT OR IGNORE INTO album_ratings (user_id, track_id, rating)
     VALUES (?, ?, ?)
   ''', (user_id, track_id, rating))
   
   connection.commit()
+
 def save_artist_rating(user_id, track_id, rating):
   cursor.execute('''
-    INSERT INTO artist_ratings (user_id, track_id, rating)
+    INSERT OR IGNORE INTO artist_ratings (user_id, track_id, rating)
     VALUES (?, ?, ?)
   ''', (user_id, track_id, rating))
+
+def save_track(user_id, track_id):
+  query = "INSERT OR IGNORE INTO listened_tracks (user_id, track_id) VALUES (?, ?)"
+  cursor.execute(query, (user_id, track_id))
+  connection.commit()
 
 def get_user_ratings(user_id):
   ratings = {}
--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,8 +1,10 @@
 import re
 import os
+import models 
 from aiogram import types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from database import save_track_rating, save_track
 
 recs = []
 current_track = None
@@ -30,93 +32,51 @@ def start(bot, dp):
             await message.reply("Подождите, ищем трек")
             from app import search_track, youtube_search, download_audio
             track = search_track(message.text)
-
+            track_id = track['id']
+            save_track(message.chat.id, track_id) 
             if track:
-                await message.reply("Ищеи видео")
+                await message.reply("Ищем")
                 youtube_query = f"{track['name']} {track['artist']}"
                 video = youtube_search(youtube_query)
                 await message.reply("Скачиваем")
                 url = f"https://youtu.be/{video['id']['videoId']}"
                 audio_path = download_audio(url)
-                await bot.send_message(chat_id=message.chat.id, text = "отправляю")
-                btn = InlineKeyboardButton("Оценить трек", callback_data="rate")
+                await bot.send_message(chat_id=message.chat.id, text = "Отправляем")
+                btn = InlineKeyboardButton("Оценить трек", callback_data=f"rate1_{track_id}") 
                 keyboard = InlineKeyboardMarkup().add(btn)
                 await bot.send_audio(chat_id=message.chat.id, audio=open(audio_path, 'rb'), reply_markup=keyboard)
                 os.remove(audio_path)
             else:
                 await message.reply("Трек не найден")
 
-    @bot.callback_query_handler(func=lambda call: call.data == "rate")
-    async def callback_rate(call):
+    @dp.callback_query_handler(lambda call: call.data.startswith("rate1"))
+    async def callback_rate(callback: types.CallbackQuery):
+        track_id = callback.data.split("_")[1]
         buttons = [
-            InlineKeyboardButton("1", callback_data=str(1)),
-            InlineKeyboardButton("2", callback_data=str(2)),
-            InlineKeyboardButton("3", callback_data=str(3)),
-            InlineKeyboardButton("4", callback_data=str(4)),
-            InlineKeyboardButton("5", callback_data=str(5))
+            InlineKeyboardButton(text, callback_data=f"rate_{track_id}_{text}") 
+            for text in ["1", "2", "3", "4", "5"]  
         ]
-        
-        await bot.edit_message_reply_markup(
-            chat_id=call.message.chat.id, 
-            message_id=call.message.message_id,
-            reply_markup=InlineKeyboardMarkup(buttons)
+        keyboard = InlineKeyboardMarkup()
+        keyboard.row(*buttons)
+        await bot.send_message(
+            chat_id=callback.message.chat.id,
+            text="Оцените от 1 до 5",
+            reply_markup=keyboard
         )
-
-    @bot.callback_query_handler(func=lambda call: call.data in ["1", "2", "3", "4", "5"])  
-    async def process_rate(callback: types.CallbackQuery):
-        user_id = callback.from_user.id
-        track_id = callback.data # id трека
-        rating = int(callback.data)
-        from database import save_track_rating
-        save_track_rating(user_id, track_id, rating)
-
-        await callback.answer()
-
-    @dp.message_handler(text="Оценить трек")
-    async def rate_tracks(message):
-
-        global current_track
-        global msg
-
-        if not current_track:  
-            current_track = recs[0]
-
-        buttons = [
-            types.InlineKeyboardButton(text, callback_data=f"rate_{text}") 
-            for text in ["1", "2", "3", "4", "5"]
-        ]
-
-        keyboard = types.InlineKeyboardMarkup().add(*buttons)
-
-        msg = await message.reply(f"Оцените трек: {current_track['track_name']}", reply_markup=keyboard) 
 
     @dp.callback_query_handler(lambda c: c.data.startswith("rate_"))
     async def rate_track(callback: types.CallbackQuery):
-
-        global current_track
-        global rated_tracks
-        global msg
-
-        # сохраняем рейтинг
-        rating = int(callback.data.split("_")[1])
-        from src.app import save_rating
-        save_rating(callback.from_user.id, current_track, rating) 
-
-        rated_tracks.append(current_track['id'])  
-        
-        buttons = [
-            types.InlineKeyboardButton(text, callback_data=f"rate_{text}") 
-            for text in ["1", "2", "3", "4", "5"]
-        ]
-        keyboard = types.InlineKeyboardMarkup().add(*buttons)
-            
-        current_index = recs.index(current_track)
-        if current_index < len(recs) - 1:
-            current_track = recs[current_index + 1]
-            await msg.edit_text(f"Оцените трек: {current_track['track_name']}")
-            await msg.edit_reply_markup(reply_markup=keyboard)
-
-        else:
-            await callback.message.reply("Оценка завершена! /start")
-
+        track_id = callback.data.split("_")[1]
+        user_id = callback.from_user.id
+        rating = int(re.search('\d+', callback.data).group()) 
+        save_track_rating(user_id, track_id, rating)
+        await bot.edit_message_reply_markup(
+            chat_id=callback.message.chat.id, 
+            message_id=callback.message.message_id,
+            reply_markup=None
+        )
+        await bot.send_message(
+            callback.message.chat.id, 
+            'Спасибо за оценку! /start'  
+        )
 
