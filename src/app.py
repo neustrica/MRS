6cdccdf7ff0126f37b6c88f63bf0fde94dad14c7
voronichgeorgiy <voronichq@gmail.com>
2023-09-11 15:06:27 +0300

    recs

--- src/app.py
+++ src/app.py
@@ -2,10 +2,16 @@ from authorization import authorize, bot, youtube
 from telegram_bot import start
 import asyncio
 from aiogram import Dispatcher
+import pandas as pd
+import models
+from sklearn.metrics.pairwise import cosine_similarity
+from database import get_ratings, get_top_rated_tracks
 import yt_dlp
 import os
 
+
 sp = authorize()
+
 
 def search_track(query):
 
@@ -19,15 +25,13 @@ def search_track(query):
       'artist': track.artists[0].name
     }
 
-def get_recommendations(track_id):
-  results = sp.recommendations(seed_tracks=[track_id], limit=5)
+def get_similar(track_id):
+  results = sp.recommendations(track_ids=track_id, limit=1)
   recommended_tracks = []
   
   for track in results.tracks:
     recommended_tracks.append({
-      'id': track.id,
-      'name': track.name,
-      'artist': track.artists[0].name
+      'id': track.id
     })
 
   return recommended_tracks
@@ -62,9 +66,72 @@ def download_audio(url):
   os.rename('temp.mp3', f'{title}.mp3')
 
   return f'{title}.mp3'
-# возвращает путь до скачанного файла
 
-dp = Dispatcher(bot)
-start(bot, dp)
-if __name__ == '__main__':
-  asyncio.run(dp.start_polling())
+# dp = Dispatcher(bot)
+# start(bot, dp)
+# if __name__ == '__main__':
+#   asyncio.run(dp.start_polling())
+
+# Функция для получения похожих пользователей
+def find_similar_users(user_id, user_sim, k=5):
+  scores = list(enumerate(user_sim[user_id]))
+  top_k = sorted(scores, key=lambda x: x[1], reverse=True)[1:k+1] 
+  return [i[0] for i in top_k]
+
+# Функция для генерации рекомендаций
+def generate_recs(user_id, k=5):
+  # Загружаем данные из БД 
+  df = get_ratings()
+  top_tracks = get_top_rated_tracks(user_id=user_id)
+  print("TOP TRACKS")
+  print(top_tracks)
+  similar_tracks = []
+  for track in top_tracks:
+    similar_tracks += get_similar(track) 
+  print("SIMILAR TRACKS")
+  print(similar_tracks)
+  user_id = int(user_id)
+  # Преобразуем в матрицу пользователей-треков
+  pivot_table = df.pivot(index='user_id', columns='track_id', values='rating').fillna(0)
+  # Вычисляем матрицу сходства    
+  user_sim = cosine_similarity(pivot_table)
+  # Находим похожих пользователей
+  similar_users = find_similar_users(user_id, user_sim, k)  
+  # Среднее рейтингов похожих пользователей 
+  scores = pivot_table.loc[similar_users].mean(axis=0)
+  # Сортируем и возвращаем топ-5
+  scores = scores.sort_values(ascending=False)[:5].index.values
+  print("SCORES")
+  print(scores)
+  # Сортируем и сохраняем рекомендации в словарь с популярностью
+  recommendations = {}
+  for track in scores:
+    popularity = sp.track(track).popularity
+    recommendations[track] = popularity
+
+  # Добавляем похожие треки в словарь рекомендаций
+  for track in similar_tracks:
+    track = track['id']
+    popularity = sp.track(track_id=track).popularity
+    recommendations[track] = popularity
+
+  # Сортируем словарь по популярности и выбираем топовые рекомендации
+  top_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)[:5]
+  recs = []
+
+  for track_id, popularity in top_recommendations:
+      track_info = sp.track(track_id)
+      track_name = track_info.name
+      artists = track_info.artists
+      artist_names = [artist.name for artist in artists]
+      
+      # Создание словаря с информацией о треке
+      track_data = {
+          'track_name': track_name,
+          'artist_names': artist_names,
+          'track_id': track_id
+      }
+      
+      # Добавление информации в список top_recommendations
+      recs.append(track_data)
+  return recs
--- src/database.py
+++ src/database.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import pandas as pd
 connection = sqlite3.connect('music_recs.db') 
 cursor = connection.cursor()
 def save_track_rating(user_id, track_id, rating):
@@ -29,9 +29,17 @@ def save_track(user_id, track_id):
   cursor.execute(query, (user_id, track_id))
   connection.commit()
 
-def get_user_ratings(user_id):
-  ratings = {}
-  
-  # получение рейтингов пользователя из всех таблиц 
-  
-  return ratings
+def get_ratings():
+  query = 'SELECT user_id, track_id, rating FROM track_ratings'
+  df = pd.DataFrame(cursor.execute(query).fetchall(), 
+                      columns=['user_id', 'track_id', 'rating'])
+  connection.commit()
+    
+  return df
+
+def get_top_rated_tracks(user_id):
+  top_rated_tracks = cursor.execute('''
+    SELECT track_id FROM track_ratings WHERE user_id = ? AND rating >= 4
+  ''', (user_id,))
+  connection.commit()
+  return top_rated_tracks
